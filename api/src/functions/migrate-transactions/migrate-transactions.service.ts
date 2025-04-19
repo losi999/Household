e65dec1ce7537017c4f7797c38ee89932bad2627
Laszlo Losonczi <>
2025-04-19 14:19:55 +0200

    Migration fix

--- api/src/functions/migrate-transactions/migrate-transactions.service.ts
+++ api/src/functions/migrate-transactions/migrate-transactions.service.ts
@@ -1,4 +1,4 @@
-import { TransactionType } from '@household/shared/enums';
+import { AccountType, TransactionType } from '@household/shared/enums';
 import { IMongodbService } from '@household/shared/services/mongodb-service';
 
 export interface IMigrateTransactionsService {
@@ -7,13 +7,40 @@ export interface IMigrateTransactionsService {
 
 export const migrateTransactionsServiceFactory = (mongodbService: IMongodbService): IMigrateTransactionsService => {
   return async () => {
+    const loanAccounts = (await mongodbService.accounts.find({
+      accountType: AccountType.Loan,
+    })).map(a => a._id);
+
     await mongodbService.transactions.updateMany({
       transactionType: 'loanTransfer',
+      transferAccount: {
+        $in: loanAccounts,
+      },
     }, [
       {
         $set: {
           transactionType: 'transfer',
           transferAmount: {
+            $multiply: [
+              '$amount',
+              -1,
+            ],
+          },
+        },
+      },
+    ]);
+
+    await mongodbService.transactions.updateMany({
+      transactionType: 'loanTransfer',
+      account: {
+        $in: loanAccounts,
+      },
+    }, [
+      {
+        $set: {
+          transactionType: 'transfer',
+          transferAmount: '$amount',
+          amount: {
             $multiply: [
               '$amount',
               -1,
