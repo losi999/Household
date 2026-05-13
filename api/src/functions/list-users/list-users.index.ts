6d06a796fff4644e7e9e7f27a5e948d08ea5fa92
losi999 <losi891021@gmail.com>
2026-05-13 18:30:36 +0200

    Parallel tests

--- api/src/functions/list-users/list-users.index.ts
+++ api/src/functions/list-users/list-users.index.ts
@@ -5,15 +5,11 @@ import { cors } from '@household/api/dependencies/handlers/cors.handler';
 import { default as index } from '@household/api/handlers/index.handler';
 import { authorizer } from '@household/api/dependencies/handlers/authorizer.handler';
 import { UserType } from '@household/shared/enums';
-import { mongoDisconnect } from '@household/api/dependencies/handlers/mongo-disconnect.handler';
 
 const listUsersService = listUsersServiceFactory(identityService);
 
 export default index({
   handler: handler(listUsersService),
   before: [authorizer(UserType.Editor)],
-  after: [
-    cors,
-    mongoDisconnect,
-  ],
+  after: [cors],
 });
