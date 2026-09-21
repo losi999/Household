2f75ec8bb7052d398a3fa1a2b460ec45209f1483
losi999 <losi891021@gmail.com>
2026-09-21 17:59:21 +0200

    household pwa

--- web/projects/household/src/app/app.config.ts
+++ web/projects/household/src/app/app.config.ts
@@ -1,4 +1,5 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
@@ -6,5 +7,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
